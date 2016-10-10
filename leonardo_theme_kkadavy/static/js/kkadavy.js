20ef6a82d0c7f165e26f13db174688dc6ca4aa42
dresl <nikicresl@gmail.com>
2016-10-10 16:44:12 +0200

    responzive menu

--- leonardo_theme_kkadavy/static/js/kkadavy.js
+++ leonardo_theme_kkadavy/static/js/kkadavy.js
@@ -5,3 +5,39 @@ $('#nav').affix({
         top: $('header').height()
       }
 });
+
+// responsive menu
+
+function DetectContainer() {
+    if ($('.navbar-static-top').width() < 700){
+        var list = document.getElementsByClassName("navbar navbar-static-top")[0];
+        var selected_div = list.getElementsByTagName('DIV')[0];
+        selected_div.classList.add('container-fluid');
+    } else {
+        var list = document.getElementsByClassName("navbar navbar-static-top")[0];
+        var selected_div = list.getElementsByTagName('DIV')[0];
+        selected_div.classList.add('container');
+    }
+}
+window.onload = function () {
+    DetectContainer();
+}
+
+window.setInterval(function(){
+    if ($('.navbar-static-top').width() < 700){
+        function ResponsiveClassContainer() {
+            var list = document.getElementsByClassName("navbar navbar-static-top")[0];
+            var selected_div = list.getElementsByTagName('DIV')[0];
+            selected_div.classList.add('container-fluid');
+        }
+        window.addEventListener("resize", ResponsiveClassContainer);
+    } else {
+        function ResponsiveClassContainer() {
+            var list = document.getElementsByClassName("navbar navbar-static-top")[0];
+            var selected_div = list.getElementsByTagName('DIV')[0];
+            selected_div.classList.remove('container-fluid');
+            selected_div.classList.add('container');
+        }
+        window.addEventListener("resize", ResponsiveClassContainer);
+    }
+}, 500);
