b9f407da4076d0cf7154ba945953c9f8af86e635
dresl <nikicresl@gmail.com>
2016-10-13 23:01:00 +0200

    update

--- leonardo_theme_kkadavy/static/js/magazine.js
+++ leonardo_theme_kkadavy/static/js/magazine.js
@@ -2,58 +2,6 @@
  * Magazine sample
 */
 
-function addPage(page, book) {
-
-	var id, pages = book.turn('pages');
-
-	// Create a new element for this page
-	var element = $('<div />', {});
-
-	// Add the page to the flipbook
-	if (book.turn('addPage', element, page)) {
-
-		// Add the initial HTML
-		// It will contain a loader indicator and a gradient
-		element.html('<div class="gradient"></div><div class="loader"></div>');
-
-		// Load the page
-		loadPage(page, element);
-	}
-
-}
-
-function loadPage(page, pageElement) {
-
-	// Create an image element
-
-	var img = $('<img />');
-
-	img.mousedown(function(e) {
-		e.preventDefault();
-	});
-
-	img.load(function() {
-		
-		// Set the size
-		$(this).css({width: '100%', height: '100%'});
-
-		// Add the image to the page after loaded
-
-		$(this).appendTo(pageElement);
-
-		// Remove the loader indicator
-		
-		pageElement.find('.loader').remove();
-	});
-
-	// Load the page
-
-	img.attr('src', 'pages/' +  page + '.jpg');
-
-	loadRegions(page, pageElement);
-
-}
-
 // Zoom in / Zoom out
 
 function zoomTo(event) {
@@ -147,40 +95,6 @@ function processRegion(region, regionType) {
 
 }
 
-// Load large page
-
-function loadLargePage(page, pageElement) {
-	
-	var img = $('<img />');
-
-	img.load(function() {
-
-		var prevImg = pageElement.find('img');
-		$(this).css({width: '100%', height: '100%'});
-		$(this).appendTo(pageElement);
-		prevImg.remove();
-		
-	});
-
-	// Loadnew page
-	
-	img.attr('src', 'pages/' +  page + '-large.jpg');
-}
-
-// Load small page
-
-function loadSmallPage(page, pageElement) {
-	
-	var img = pageElement.find('img');
-
-	img.css({width: '100%', height: '100%'});
-
-	img.unbind('load');
-	// Loadnew page
-
-	img.attr('src', 'pages/' +  page + '.jpg');
-}
-
 // http://code.google.com/p/chromium/issues/detail?id=128488
 
 function isChrome() {
@@ -212,7 +126,7 @@ function resizeViewport() {
 	$('.magazine').removeClass('animated');
 
 	$('.magazine-viewport').css({
-		width: width,
+		width: '100%',
 		height: height
 	}).
 	zoom('resize');
